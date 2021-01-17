ef3641772ddb1281c89b253269700d16601ec332
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-01-17 16:20:33 +0000

    small changes made

--- src/TeenNumCheck.java
+++ src/TeenNumCheck.java
@@ -1,36 +1,7 @@
 public class TeenNumCheck {
-    public static void main(String[] args) {
 
-        //declaring a boolean variable
-        boolean evaluate;
-
-        //passing data for first condition
-        evaluate= hasTeen(9,99,19);
-        //printing out result
-        System.out.println(evaluate);
-
-        //passing data for second condition
-        evaluate = hasTeen(23,15,42);
-        //printing out result
-        System.out.println(evaluate);
-
-        //passing data for second condition
-        evaluate = hasTeen(22,23,43);
-        //printing out result
-        System.out.println(evaluate);
-
-        //passing data for fourth condition
-        evaluate = isTeen(13);
-        //printing out result
-        System.out.println(evaluate);
-
-    }
     public static boolean hasTeen(int first, int second, int third){
-
-        //conditional statement to check data
-        //returns true when conditions are met
        if (first >= 13 && first <= 19 || second >= 13 && second <= 19 || third >= 13 && third <= 19){
-
            return true;
         }
        else{
@@ -45,5 +16,5 @@ public class TeenNumCheck {
             return false;
         }
     }
-
 }
+//Victor-Aremu
